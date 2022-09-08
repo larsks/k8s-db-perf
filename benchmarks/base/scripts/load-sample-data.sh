6879568ffdf54f7d36466dc8a9bbdd5e3a2a18ed
Lars Kellogg-Stedman <lars@oddbit.com>
2022-09-07 22:56:03 -0400

    Add some logging around data import

--- benchmarks/base/scripts/load-sample-data.sh
+++ benchmarks/base/scripts/load-sample-data.sh
@@ -5,5 +5,6 @@ set -eu
 git clone https://github.com/datacharmer/test_db  /tmp/test_db
 
 cd /tmp/test_db
-set -x
+echo "start loading data"
 mysql -u"root" -p"${MARIADB_ROOT_PASSWORD}" "${MARIADB_DATABASE}" < employees.sql
+echo "finished loading data"
